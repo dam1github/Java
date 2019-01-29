cafa124632bf997c9661f52237c7260c231d6985
dam1github <44295876+dam1github@users.noreply.github.com>
2019-01-29 19:23:13 +0100

    Update subjectsClass.java

--- Tema 6/EJ_08/src/ej_08/subjectsClass.java
+++ Tema 6/EJ_08/src/ej_08/subjectsClass.java
@@ -2,23 +2,5 @@ package ej_08;
 import java.util.*;
 
 public class subjectsClass {
-	public int subjectID;
-	public String subjectName;
-	
-	public subjectsClass(int subjectID, String subjectName) {
-		this.subjectID = subjectID;
-		this.subjectName = subjectName;
-	}
-	
-/*	public String getSubject(int subjectID) {
-		String subjectNameByID;
-		if(subjectID == 1)
-			subjectNameByID = firstSubjectName;
-		else if(subjectID == 2)
-			subjectNameByID = secondSubjectName;
-		else 
-			subjectNameByID = null;
-		
-		return subjectNameByID;
-	}*/
+	// Paso de hacer este ejercicio, está fatal explicado TODO. No se que tengo que hacer.
 }
